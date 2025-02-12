fffc6d84092c5978f7dd561dccf0ea37b0c0a9e6
Wendy <awendy.aguilar@gmail.com>
2025-02-11 22:11:56 -0800

    initial vite app + Material UI

---
+++ spa/src/App.tsx
@@ -0,0 +1,14 @@
+import { Card, CardContent, Container } from "@mui/material";
+function App() {
+  return (
+    <>
+      <Container maxWidth="lg" fixed>
+        <Card>
+          <CardContent>Hello Maiven</CardContent>
+        </Card>
+      </Container>
+    </>
+  );
+}
+
+export default App;
