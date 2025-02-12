74c19f1a980b3ef75b8833a9e7cd6c43f6b40c63
Wendy <awendy.aguilar@gmail.com>
2025-02-11 22:33:41 -0800

    added user and message resources

---
+++ api/src/messages/messages.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { MessagesService } from './messages.service';
+import { CreateMessageDto } from './dto/create-message.dto';
+
+@Controller('messages')
+export class MessagesController {
+  constructor(private readonly messagesService: MessagesService) {}
+
+  @Post()
+  create(@Body() createMessageDto: CreateMessageDto) {
+    return this.messagesService.create(createMessageDto);
+  }
+
+  @Get(':id')
+  findAllById(@Param('id') id: number) {
+    return this.messagesService.findAllById(+id);
+  }
+}
